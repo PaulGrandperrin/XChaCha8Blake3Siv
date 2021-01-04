e3831ef47f4efb72616766c25143b3bca4def917
Paul Grandperrin <paul.grandperrin@gmail.com>
2021-01-04 15:14:01 -0300

    add some doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,39 @@
 /*
-// encrypt(key:256, iv:192, ad:*, plaintext:*) -> tag:256, ciphertext:*
-let tag:256 = blake3::keyed_hash(key:256, iv:192 + ad:* + plaintext:*)
-let siv:192 = tag[0..192]
-let ciphertext:* = xchacha20(key:256, siv:192, plaintext:*)
+XChaCha20Blake3Siv is a nonce-reuse misuse-resistant (NRMR) and key-commiting authenticated encryption with associated data (AEAD) algorithm .
+
+XChaCha20Blake3Siv can also be used as a deterministic authenticated encryption (DAE) key-commiting AEAD.
+
+XChaCha20Blake3Siv is inspired by the generic Synthetic Initialization Vector (SIV) construction described in [0].
+
+We assume XChaCha20Blake3Siv is key-commiting and therefore resistant to partitioning oracle attacks based on our understanding of [1].
+
+We believe that using the Mac-then-Encrypt (MtE) is secure because we are not using a block cipher, so there is no padding
+and so no padding oracle attack is possible.
+
+We don't perform key separation between the cipher and the PRF because we assume that xchacha20 and blake3 are sufficiently unrelated. 
+
+0: https://datatracker.ietf.org/doc/draft-madden-generalised-siv/
+1: https://www.usenix.org/conference/usenixsecurity21/presentation/len
+
+Pseudocode:
+
+fn encrypt(key:256, iv:192, ad:*, plaintext:*) -> tag:256, ciphertext:*
+  let tag:256 = blake3::keyed_hash(key:256, iv:192 + ad:* + plaintext:*)
+  let siv:192 = tag[0..192]
+  let ciphertext:* = xchacha20(key:256, siv:192, plaintext:*)
 
 
-//decrypt(key:256, iv:192, tag:256, ad:*, ciphertext:*)
-let siv:192 = tag[0..192]
-let plaintext:* = xchacha20(key:256, siv:192, ciphertext:*)
-let tag2 = blake3::keyed_hash(key:256, iv:192 + ad:* + plaintext:*)
-assert!(tag == tag2) // constant time
+fn decrypt(key:256, iv:192, tag:256, ad:*, ciphertext:*)
+  let siv:192 = tag[0..192]
+  let plaintext:* = xchacha20(key:256, siv:192, ciphertext:*)
+  let tag2 = blake3::keyed_hash(key:256, iv:192 + ad:* + plaintext:*)
+  assert!(tag == tag2) // constant time
+
 */
 
 use aead::{AeadInPlace, Error, NewAead, consts::{U0, U24, U32}, generic_array::GenericArray};
 use typenum::Unsigned;
-use chacha20::cipher::{NewStreamCipher, SyncStreamCipher};
+use chacha20::{XChaCha20, cipher::{NewStreamCipher, SyncStreamCipher}};
 use zeroize::Zeroize;
 
 pub type Key = GenericArray<u8, <XChaCha20Blake3Siv as NewAead>::KeySize>;
@@ -50,7 +69,7 @@ impl AeadInPlace for XChaCha20Blake3Siv {
         hasher.update(buffer);
         let tag: Tag = Into::<[u8; Self::TagSize::USIZE]>::into(hasher.finalize()).into(); // consumes the Hash to avoid copying
         let siv: &Nonce = tag.as_slice()[0..Self::NonceSize::USIZE].into(); // constructs a reference to avoid copying
-        chacha20::XChaCha20::new(&self.key,siv).apply_keystream(buffer);
+        XChaCha20::new(&self.key,siv).apply_keystream(buffer);
         Ok(tag)
     }
 
@@ -62,7 +81,7 @@ impl AeadInPlace for XChaCha20Blake3Siv {
         tag: &Tag,
     ) -> Result<(), Error> {
         let siv: &Nonce = tag.as_slice()[0..Self::NonceSize::USIZE].into(); // constructs a reference to avoid copying
-        chacha20::XChaCha20::new(&self.key,siv).apply_keystream(buffer);
+        XChaCha20::new(&self.key,siv).apply_keystream(buffer);
         let mut hasher = blake3::Hasher::new_keyed(self.key.as_ref());
         hasher.update(nonce);
         hasher.update(associated_data);
