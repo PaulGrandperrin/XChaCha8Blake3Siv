8ee6849973427be6223106276bb0e7bdcf13a0c9
Paul Grandperrin <paul.grandperrin@gmail.com>
2021-01-04 17:59:11 -0300

    bench: compare against ChaChaPoly1305::<c2_chacha::Ietf> so both are based on c2-chacha

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,10 +1,10 @@
-use aead::{NewAead, AeadInPlace};
-use chacha20poly1305::XChaCha20Poly1305;
+use aead::{AeadInPlace, NewAead, Nonce};
+use chacha20poly1305::ChaChaPoly1305;
 use criterion::{BenchmarkId, Throughput};
 use criterion::Criterion;
 use criterion::{criterion_group, criterion_main};
 use rand::Rng;
-use xchacha20blake3siv::{Key, Nonce, XChaCha20Blake3Siv};
+use xchacha20blake3siv::{Key, XChaCha20Blake3Siv};
 
 
 fn bench(c: &mut Criterion) {
@@ -14,10 +14,11 @@ fn bench(c: &mut Criterion) {
     rand::thread_rng().fill(&mut buffer[..]);
 
     let key = Key::from_slice(b"an example very very secret key."); // 32-bytes
-    let nonce = Nonce::from_slice(b"extra long unique nonce!"); // 24-bytes; unique per message
+    let nonce12 = Nonce::from_slice(b"unique nonce"); // 12-bytes; unique per message
+    let nonce24 = Nonce::from_slice(b"extra long unique nonce!"); // 24-bytes; unique per message
     let associated_data = b"";
     let cipher_xchacha20blake3siv = XChaCha20Blake3Siv::new(key);
-    let cipher_xchacha20poly1305 = XChaCha20Poly1305::new(key);
+    let cipher_chacha20poly1305 = ChaChaPoly1305::<c2_chacha::Ietf>::new(key.into());
 
     let mut group_xchacha20blake3siv = c.benchmark_group("xchacha20blake3siv");
     for size in [1, 32, 128, 4 * KB, 64 * KB].iter() {
@@ -25,29 +26,29 @@ fn bench(c: &mut Criterion) {
 
         group_xchacha20blake3siv.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
             b.iter(|| {
-                let tag = cipher_xchacha20blake3siv.encrypt_in_place_detached(nonce, associated_data, &mut buffer[0..size])
+                let tag = cipher_xchacha20blake3siv.encrypt_in_place_detached(nonce24, associated_data, &mut buffer[0..size])
                 .   expect("encryption failure!");
-                cipher_xchacha20blake3siv.decrypt_in_place_detached(nonce, associated_data, &mut buffer[0..size], &tag)
+                cipher_xchacha20blake3siv.decrypt_in_place_detached(nonce24, associated_data, &mut buffer[0..size], &tag)
                     .expect("decryption failure!");
             });
         });
     }
     group_xchacha20blake3siv.finish();
 
-    let mut group_xchacha20poly1305 = c.benchmark_group("xchacha20poly1305");
+    let mut group_chacha20poly1305 = c.benchmark_group("chacha20poly1305");
     for size in [1, 32, 128, 4 * KB, 64 * KB].iter() {
-        group_xchacha20poly1305.throughput(Throughput::Bytes(*size as u64));
+        group_chacha20poly1305.throughput(Throughput::Bytes(*size as u64));
 
-        group_xchacha20poly1305.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
+        group_chacha20poly1305.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
             b.iter(|| {
-                let tag = cipher_xchacha20poly1305.encrypt_in_place_detached(nonce, associated_data, &mut buffer[0..size])
+                let tag = cipher_chacha20poly1305.encrypt_in_place_detached(nonce12, associated_data, &mut buffer[0..size])
                 .   expect("encryption failure!");
-                cipher_xchacha20poly1305.decrypt_in_place_detached(nonce, associated_data, &mut buffer[0..size], &tag)
+                cipher_chacha20poly1305.decrypt_in_place_detached(nonce12, associated_data, &mut buffer[0..size], &tag)
                     .expect("decryption failure!");
             });
         });
     }
-    group_xchacha20poly1305.finish();
+    group_chacha20poly1305.finish();
 
 }
 
