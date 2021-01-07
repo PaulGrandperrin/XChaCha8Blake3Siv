bde504d185bdcb3057349b1b10bc637ebdadcfb8
Paul Grandperrin <paul.grandperrin@gmail.com>
2021-01-07 13:06:43 -0300

    add Blake3StreamCipher

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,14 +1,40 @@
 use aead::{AeadInPlace, Key, NewAead, Nonce};
 use chacha20poly1305::{ChaChaPoly1305, XChaCha20Poly1305};
+use cipher::{NewStreamCipher, StreamCipher};
 use criterion::{BenchmarkId, Throughput};
 use criterion::Criterion;
 use criterion::{criterion_group, criterion_main};
 use rand::Rng;
 use xchacha8blake3siv::AeadSiv;
-use typenum::Unsigned;
+use typenum::{U32, Unsigned};
 
 const KB: usize = 1024;
 const MB: usize = 1024 * KB;
+
+struct Blake3StreamCipher {
+    xof: blake3::OutputReader
+}
+
+impl NewStreamCipher for Blake3StreamCipher {
+    type KeySize = U32;
+    type NonceSize = U32;
+
+    fn new(key: &c2_chacha::stream_cipher::Key<Self>, nonce: &c2_chacha::stream_cipher::Nonce<Self>) -> Self {
+        Self {
+            xof: blake3::Hasher::new_keyed(key.as_ref()).update(nonce).finalize_xof()
+        }
+    }
+}
+
+impl StreamCipher for Blake3StreamCipher {
+    fn encrypt(&mut self, data: &mut [u8]) {
+        self.xof.xor(data);
+    }
+
+    fn decrypt(&mut self, data: &mut [u8]) {
+        self.xof.xor(data);
+    }
+}
 
 fn bench_aead<A: NewAead + AeadInPlace>(c: &mut Criterion, name: &str) {
     let mut buffer = vec![0u8; MB];
@@ -38,13 +64,16 @@ fn bench_aead<A: NewAead + AeadInPlace>(c: &mut Criterion, name: &str) {
 fn bench(c: &mut Criterion) {
     bench_aead::<XChaCha20Poly1305>(c, "XChaCha20Poly1305");
     bench_aead::<ChaChaPoly1305<c2_chacha::Ietf>>(c, "ChaChaPoly1305<c2_chacha::Ietf>");
-    bench_aead::<AeadSiv<c2_chacha::Ietf, blake3::Hasher>>(c, "c2_chacha::Ietf, blake3::Hasher>");
-    bench_aead::<AeadSiv<c2_chacha::ChaCha8, blake3::Hasher>>(c, "c2_chacha::ChaCha8, blake3::Hasher>");
-    bench_aead::<AeadSiv<c2_chacha::ChaCha12, blake3::Hasher>>(c, "c2_chacha::ChaCha12, blake3::Hasher>");
-    bench_aead::<AeadSiv<c2_chacha::ChaCha20, blake3::Hasher>>(c, "c2_chacha::ChaCha20, blake3::Hasher>");
-    bench_aead::<AeadSiv<c2_chacha::XChaCha8, blake3::Hasher>>(c, "c2_chacha::XChaCha8, blake3::Hasher>");
-    bench_aead::<AeadSiv<c2_chacha::XChaCha12, blake3::Hasher>>(c, "c2_chacha::XChaCha12, blake3::Hasher>");
-    bench_aead::<AeadSiv<c2_chacha::XChaCha20, blake3::Hasher>>(c, "c2_chacha::XChaCha20, blake3::Hasher>");
+
+    bench_aead::<AeadSiv<c2_chacha::Ietf, blake3::Hasher>>(c, "AeadSiv<c2_chacha::Ietf, blake3::Hasher>");
+    bench_aead::<AeadSiv<c2_chacha::ChaCha8, blake3::Hasher>>(c, "AeadSiv<c2_chacha::ChaCha8, blake3::Hasher>");
+    bench_aead::<AeadSiv<c2_chacha::ChaCha12, blake3::Hasher>>(c, "AeadSiv<c2_chacha::ChaCha12, blake3::Hasher>");
+    bench_aead::<AeadSiv<c2_chacha::ChaCha20, blake3::Hasher>>(c, "AeadSiv<c2_chacha::ChaCha20, blake3::Hasher>");
+    bench_aead::<AeadSiv<c2_chacha::XChaCha8, blake3::Hasher>>(c, "AeadSiv<c2_chacha::XChaCha8, blake3::Hasher>");
+    bench_aead::<AeadSiv<c2_chacha::XChaCha12, blake3::Hasher>>(c, "AeadSiv<c2_chacha::XChaCha12, blake3::Hasher>");
+    bench_aead::<AeadSiv<c2_chacha::XChaCha20, blake3::Hasher>>(c, "AeadSiv<c2_chacha::XChaCha20, blake3::Hasher>");
+    
+    bench_aead::<AeadSiv<Blake3StreamCipher, blake3::Hasher>>(c, "AeadSiv<Blake3StreamCipher, blake3::Hasher>");
 }
 
 criterion_group!(benches, bench);
